905a118296c09268001006a1628d9b301fdf5c70
AlexRuu <weiru@hotmail.ca>
2022-12-02 12:02:00 -0500

    Change Router base name

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div>
-      <Router>
+      <Router basename="/">
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
