252e8240894815c16021a1f7cbb6d52e9a539781
AlexRu <weiru@hotmail.ca>
2022-12-02 01:09:00 -0500

    Add error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <h1>Error</h1>;
+  return (
+    <section className="error-page section">
+      <div className="error-container">
+        <h1>The page you are looking for does not exist...</h1>
+        <Link to="/" className="btn btn-primary">
+          Go back
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Error;
